5dd78f8be55a7a46e0f54367d08de8029823ec94
Rica0 <ricardonkodia1999@gmail.com>
2022-10-24 16:52:39 +0200

    tout mon projet

---
+++ main.js
@@ -0,0 +1,14 @@
+let couleur1 = document.querySelector("couleur1");
+let couleur2 = document.querySelector("couleur2");
+let couleur3 = document.querySelector("couleur3");
+let image = document.querySelector("image-moov")
+
+couleur1.addEventListener{'click', {} =>{
+	image.src ="b514b0f0-93e0-4f5b-8437-b2f6390f96b7.JPG";
+}};
+couleur2.addEventListener{'click', {} =>{
+	image.src ="house-g5342c1b2f_1280.jpg";
+}};
+couleur3.addEventListener{'click', {} =>{
+	image.src ="wall-gbd9a6ddd9_1280.jpg";
+}};
